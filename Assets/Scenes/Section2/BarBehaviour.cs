040c56309710d4a4ce63b770b0cf60d78b967c39
genki-ist <genki.toyama@i-studio.co.jp>
2020-12-12 19:00:58 +0900

    add Section2 sample

--- Assets/Scenes/Section2/BarBehaviour.cs
+++ Assets/Scenes/Section2/BarBehaviour.cs
@@ -1,21 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zenject;
 
 namespace App.Section2
 {
     public class BarBehaviour : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-            
-        }
+        //フィールドインジェクション
+        // [Inject]
+        // private FooBehaviour _fooBehaviour;
 
-        // Update is called once per frame
-        void Update()
+        //プロパティインジェクション
+        // [Inject]
+        // public FooBehaviour FooBehaviourProperty
+        // {
+        //     get;
+        //     private set;
+        // }
+
+        //インスペクタ確認用にあえてpublic
+        public FooBehaviour fooBehaviour;
+
+        //メソッドインジェクション
+        //DiContainerにBindしたComponent(FooBehaviour)をContextがinjectする
+        [Inject]
+        public void Construct(FooBehaviour fooBehaviour)
         {
-            
+            this.fooBehaviour = fooBehaviour;
         }
     }
 }
